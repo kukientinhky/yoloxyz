fd45fea4913b665756be2d403b9ce20bfcd01ec1
kiên lê <lexuankien122@gmail.com>
2024-05-10 09:53:48 +0700

    return check data

--- yoloxyz/train.py
+++ yoloxyz/train.py
@@ -110,8 +110,8 @@ def train(hyp, opt, device, tb_writer=None):
         logger.info('\n\nTransferred %g/%g items from %s\n\n' % (len(state_dict), len(model.state_dict()), weights))  # report
     else:
         model = Model(opt.cfg, ch=3, nc=nc, anchors=hyp.get('anchors')).to(device)  # create
-    # with torch_distributed_zero_first(rank):
-    #     check_dataset(data_dict)  # check
+    with torch_distributed_zero_first(rank):
+        check_dataset(data_dict)  # check
     train_path = data_dict['train']
     test_path = data_dict['val']
 
